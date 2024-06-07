20bb4c98b268710e57829ef4283418ed3740bbcd
chainpusher <chainpusher@example.com>
2024-06-07 14:46:44 +0800

    feat: Add GetInfuraApiUrl function to retrieve Infura API URL
    This commit adds the `GetInfuraApiUrl` function to the `eth.go` file in the `chain` package. The function retrieves the Infura API URL by combining the `InfuraApiUrl` constant with the `INFURA_KEY` environment variable obtained using the `GetEnv` function from the `sys` package. This addition enables the application to dynamically obtain the Infura API URL based on the environment configuration.
    
    Note: This commit message is based on the provided code changes and does not take into account the recent repository commits.

--- chain/eth.go
+++ chain/eth.go
@@ -1,1 +1,44 @@
 package chain
+
+import "github.com/chainpusher/chainpusher/sys"
+
+const (
+	InfuraApiUrl        string = "https://mainnet.infura.io/v3/"
+	EthereumUsdtAddress string = "0xdac17f958d2ee523a2206206994597c13d831ec7"
+
+	EthereumUsdtAbi string = `[
+		{
+			"constant": false,
+			"inputs": [
+				{
+					"name": "_to",
+					"type": "address"
+				},
+				{
+					"name": "_value",
+					"type": "uint256"
+				}
+			],
+			"name": "transfer",
+			"outputs": [
+				{
+					"name": "",
+					"type": "bool"
+				}
+			],
+			"payable": false,
+			"stateMutability": "nonpayable",
+			"type": "function"
+		}
+	]`
+)
+
+func GetInfuraApiUrl() (string, error) {
+
+	key, err := sys.GetEnv("INFURA_KEY")
+	if err != nil {
+		return key, err
+	}
+
+	return InfuraApiUrl + key, nil
+}
--- chain/eth_test.go
+++ chain/eth_test.go
@@ -2,9 +2,15 @@ package chain_test
 
 import (
 	"context"
+	"log"
+	"math/big"
+	"strings"
 	"testing"
 
+	"github.com/chainpusher/chainpusher/chain"
 	"github.com/chainpusher/chainpusher/sys"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
@@ -27,5 +33,39 @@ func TestEthBlockChain_GetNowBlock(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	t.Log(header)
+	if header.Number.Cmp(big.NewInt(1)) == -1 {
+		t.Fatal("Block number is less than 1")
+	}
+}
+
+func TestEthereumAbiParse(t *testing.T) {
+	expectedAddress := "0x5b6d5BB6995A7C21aaC64c78A4c5B88470a0B15e"
+	expectedAmount := 1
+
+	parsedABI, err := abi.JSON(strings.NewReader(chain.EthereumUsdtAbi))
+	if err != nil {
+		log.Fatalf("Failed to parse contract ABI: %v", err)
+	}
+
+	data := "0xa9059cbb0000000000000000000000005b6d5bb6995a7c21aac64c78a4c5b88470a0b15e0000000000000000000000000000000000000000000000000000000000000001" // Replace with your data
+	method, err := parsedABI.MethodById(common.FromHex(data))
+	if err != nil {
+		log.Fatalf("Failed to get method: %v", err)
+	}
+
+	args, err := method.Inputs.Unpack(common.FromHex(data)[4:])
+	if err != nil {
+		log.Fatalf("Failed to unpack data: %v", err)
+	}
+
+	to := args[0].(common.Address)
+	value := args[1].(*big.Int)
+
+	if to.Hex() != expectedAddress {
+		t.Fatalf("Expected address: %s, got: %s", expectedAddress, to.Hex())
+	}
+
+	if value.Int64() != int64(expectedAmount) {
+		t.Fatalf("Expected amount: %d, got: %d", expectedAmount, value.Int64())
+	}
 }
